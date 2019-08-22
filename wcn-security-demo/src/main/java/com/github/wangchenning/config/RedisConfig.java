29b1c8d6e59ce111dac576f33ae9e13ac302d187
G-ChenNing <wcngoodlight@gmail.com>
2019-08-22 11:53:13 +0800

    update

--- wcn-security-demo/src/main/java/com/github/wangchenning/config/RedisConfig.java
+++ wcn-security-demo/src/main/java/com/github/wangchenning/config/RedisConfig.java
@@ -1,21 +1,12 @@
 package com.github.wangchenning.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.wangchenning.security.core.validate.code.ValidateCode;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
 
 import java.net.UnknownHostException;
 
